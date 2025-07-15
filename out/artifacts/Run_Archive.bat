ede7247d57d49efaadb3f4a002ba58fa2aae9d96
MENON1939 <ryotaro1985@outlook.com>
2025-07-15 20:54:03 +0900

    Ignored some files

--- out/artifacts/Run_Archive.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set ZIP_EXE="C:\Program Files\7-Zip\7z.exe"
-set TARGET_DIR="Japan_Route_Finder_jar"
-set OUTPUT_ZIP="Japan-Route-Finder.zip"
-
-%ZIP_EXE% a -tzip %OUTPUT_ZIP% %TARGET_DIR%
-pause
