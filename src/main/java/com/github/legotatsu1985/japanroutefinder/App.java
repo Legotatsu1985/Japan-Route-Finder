4df2b17fb441a378530fff08666583ab64f0058f
MENON1939 <ryotaro1985@outlook.com>
2025-11-27 10:16:41 +0900

    Swing LookAndFeel is now set FlatLaf as default.

--- src/main/java/com/github/legotatsu1985/japanroutefinder/App.java
+++ src/main/java/com/github/legotatsu1985/japanroutefinder/App.java
@@ -1,7 +1,12 @@
 package com.github.legotatsu1985.japanroutefinder;
 
+import com.formdev.flatlaf.*;
+
 import com.github.legotatsu1985.japanroutefinder.ui.MainWindow;
 import com.github.legotatsu1985.japanroutefinder.components.FilesController;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -12,7 +17,15 @@ public class App {
             System.out.println("Creating default properties file...");
             FilesController.createPropertiesFile(APP_CONFIG_PROPERTIES_PATH);
         }
-        MainWindow mainWindow = new MainWindow();
-        mainWindow.setVisible(true);
+        // MainWindow mainWindow = new MainWindow();
+        // mainWindow.setVisible(true);
+        UIManager.setLookAndFeel(new FlatLightLaf());
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new MainWindow().setVisible(true);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
