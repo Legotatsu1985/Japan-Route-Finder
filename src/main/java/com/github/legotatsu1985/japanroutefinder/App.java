8d6b45ac416fa00fb759250405783e1f8d453bfd
Ryotaro-Yamanaka <25011121@c.anabuki-college.net>
2026-04-23 10:15:54 +0900

    Never create an instance of JFrame. FilesController is no longer used.

--- src/main/java/com/github/legotatsu1985/japanroutefinder/App.java
+++ src/main/java/com/github/legotatsu1985/japanroutefinder/App.java
@@ -1,43 +1,20 @@
 package com.github.legotatsu1985.japanroutefinder;
 
+import com.github.legotatsu1985.japanroutefinder.handlers.*;
 import com.github.legotatsu1985.japanroutefinder.ui.*;
 import com.github.legotatsu1985.japanroutefinder.util.*;
 
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.SwingUtilities;
 import java.awt.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class App {
     public static final String APP_CONFIG_PROPERTIES_PATH = "config.properties";
-    public static LangJsonLoader LANG;
-    public static final FilesController FILES_CONTROLLER = new FilesController(APP_CONFIG_PROPERTIES_PATH);
+    public static LangHandler LANG_HANDLER;
+    public static final Config CFG = new Config(APP_CONFIG_PROPERTIES_PATH);
+    public static UIHandler UI;
     public static void main(String[] args) {
-        if (!Files.exists(Paths.get(APP_CONFIG_PROPERTIES_PATH))) {
-            System.out.println("Creating default properties file...");
-            FILES_CONTROLLER.initProperties();
-        }
-        String langCode = LangJsonLoader.checkLang();
-        LANG = new LangJsonLoader(langCode);
-        // MainWindow mainWindow = new MainWindow();
-        // mainWindow.setVisible(true);
-        try {
-            UIManager.setLookAndFeel(SettingsWindow.getWindowStyle());
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-        SwingUtilities.invokeLater(() -> new MainWindow().setVisible(true));
-    }
-    public static void updateLookAndFeel() {
-        try {
-            UIManager.setLookAndFeel(SettingsWindow.getWindowStyle());
-            for (Window window : Window.getWindows()) {
-                SwingUtilities.updateComponentTreeUI(window);
-            }
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
+        String langCode = CFG.getLangCode();
+        LANG_HANDLER = new LangHandler(langCode);
+        UI = new UIHandler();
+        UI.openMain();
     }
 }
